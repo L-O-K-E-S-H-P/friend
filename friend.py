20aec9bc24104b8113f5dc8926fba6f9927b8ae8
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2021-08-21 19:28:28 +0530

    Update friend.py

--- friend.py
+++ friend.py
@@ -1,4 +1,4 @@
 import pyttsx3
 friend = pyttsx3.init()
-friend.say("Electric car maker Tesla Inc plans to introduce a new low-cost, long-life battery in its Model 3 sedan in China later this year or early next that it expects will bring the cost of electric vehicles in line with gasoline models, and allow EV batteries to have second and third lives in the electric power grid.")
+friend.say("Electric car maker Tesla Inc plans to introduce a new low-cost, long-life battery in its Model sedan in China later this year or early next that it expects will bring the cost of electric vehicles in line with gasoline models, and allow EV batteries to have second and third lives in the electric power grid.")
 friend.runAndWait()
